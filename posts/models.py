0f3fcb51bdab4f7af97c1dfd1a5cc89bc88d1b8d
yuspace22 <yuspace22@gmail.com>
2025-06-06 16:53:42 +0800

    Init Django.

---
+++ posts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+# 物件→藍圖
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-pub_date']
+    
+    def __str__(self):
+        return self.title
+
+# 實例→實體化
