f865bc62f17e57d66e5cabd6fe50624b79511ba5
yus <yuspace22@gmail.com>
2025-06-27 09:56:38 +0800

    ad 註解

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-
+# 已安裝的套件
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -52,6 +52,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "DjangoBlog.urls"
 
+
+# 指定模板的位置
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -74,7 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "DjangoBlog.wsgi.application"
 
 
-# Database
+# Database 資料庫
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
@@ -104,10 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Internationalization 設定語言和時區
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "zh-hant"
+LANGUAGE_CODE = "zh-hant" 
 
 TIME_ZONE = "Asia/Taipei"
 
@@ -116,7 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) 將檔案放入Django
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
